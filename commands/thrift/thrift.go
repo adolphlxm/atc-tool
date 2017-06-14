9498f19e6fc51eb3f6dbb73279aac6e00f0d6ed2
adolphlxm <adolph.liu@aliyun.com>
2017-06-14 19:49:32 +0800

    Fix the bug for thrift

--- commands/thrift/thrift.go
+++ commands/thrift/thrift.go
@@ -31,10 +31,12 @@ func Run(cmd *commands.Command, args []string) int {
 
 	// Replace thrift package
 	gen := args[3]
-	i := strings.LastIndex(gen, "/")
-	thriftGenName := gen[i+1 : i+6]
+	i1 := strings.LastIndex(gen, "/")
+	i2 := strings.LastIndex(gen, ".")
+	thriftGenName := gen[i1+1:i2]
+
 	helper := utils.ReplaceHelper{
-		Root:    "./gen-go-go/" + thriftGenName,
+		Root:    "./gen-go/" + thriftGenName,
 		OldText: "git.apache.org/thrift.git/lib/go/thrif",
 		NewText: "github.com/adolphlxm/atc/rpc/thrift/lib/thrif",
 	}
